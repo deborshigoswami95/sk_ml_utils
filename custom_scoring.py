65d3e630161162e5051a07f0105d19ea55786843
Deborshi Goswami <33585773+deborshigoswami95@users.noreply.github.com>
2022-06-14 16:30:34 -0700

    Create custom_scoring.py

---
+++ custom_scoring.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.scorer import make_scorer
+
+def get_custom_scorer(scorer,greater_is_better):
+  return make_scorer(scorer,greater_is_better)
+
+
+
+def MAPE(y_pred,y_true):
+  np.mean(abs(y_pred-y_true)*100/y_true)
+    
+  
+if __name__=="__main__":
+  print(f"{__file__} called")
